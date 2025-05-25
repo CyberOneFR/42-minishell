6282ee88875a70887f05b4b1e05d26eeb3f35dc0
CyberOneFR <erwoan.thebault@gmail.com>
2025-05-25 04:52:25 +0200

    updated

--- headers/types/tnode.h
+++ headers/types/tnode.h
@@ -6,13 +6,19 @@
 /*   By: ethebaul <ethebaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 04:48:37 by ethebaul          #+#    #+#             */
-/*   Updated: 2025/05/25 04:48:52 by ethebaul         ###   ########.fr       */
+/*   Updated: 2025/05/25 04:52:02 by ethebaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TNODE_H
 # define TNODE_H
 
+# include "tarray.h"
 
+typedef struct s_node
+{
+	struct s_node	*parent;
+	t_array			childs;
+}	t_node;
 
 #endif
